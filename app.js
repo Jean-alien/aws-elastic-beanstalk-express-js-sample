7980fd9b18b288c5aee5b30257e359d831d0f5b7
Myeongji An <marvel0855@gmail.com>
2024-04-20 04:17:22 -0500

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('1100 SW 66th, Oklahoma City, OK 73139'));
+app.get('/', (req, res) => res.send('There are 5 rule actions by default while you run a typical Snort rule'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
